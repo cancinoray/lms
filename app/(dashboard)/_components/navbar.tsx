fd93b8d301a37646dad70c8a51347602540385a6
Ray Cancino <cancinoray@gmail.com>
2023-10-27 02:38:26 +0800

    added sheets from shadcn

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,10 @@
+import { MobileSidebar } from "./mobile-sidebar";
+
+export const Navbar = () => {
+  return (
+    <div className="flex items-center h-full p-4 bg-white border-b shadow-sm">
+      <MobileSidebar />
+      {/* <NavbarRoutes /> */}
+    </div>
+  );
+};
